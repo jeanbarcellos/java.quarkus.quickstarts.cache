f1959f12ea9875c53fc20a0c5de9c4cd482f6704
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-06-11 19:42:58 -0300

    Configuração do cache

---
+++ src/main/java/org/acme/cache/CacheResource.java
@@ -0,0 +1,50 @@
+package org.acme.cache;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cache")
+@Produces(MediaType.TEXT_PLAIN)
+public class CacheResource {
+
+    private static final Object OBJ = new Object();
+
+    @Inject
+    MeuServicoDemorado servico;
+
+    @GET
+    @Path("{id}")
+    public String primeiro(@PathParam("id") String id) {
+        return servico.primeiroCacheavel(id, new Object());
+    }
+
+    @GET
+    @Path("mesmo-objeto/{id}")
+    public String segundo(@PathParam("id") String id) {
+        return servico.primeiroCacheavel(id, OBJ);
+    }
+
+    @DELETE
+    @Path("tudo")
+    public void limpeTudo() {
+        servico.limpePrimeiroCacheTodo();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void limpeUm(@PathParam("id") String id) {
+        servico.limpePrimeiroCache(id, OBJ);
+    }
+
+    @GET
+    @Path("put-mesmo-objeto/{id}")
+    public String segundoCache(@PathParam("id") String id) {
+        return servico.segundoCacheavel(id, OBJ);
+    }
+
+}
---
+++ src/main/java/org/acme/cache/MeuServicoDemorado.java
@@ -0,0 +1,48 @@
+package org.acme.cache;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.quarkus.cache.CacheInvalidate;
+import io.quarkus.cache.CacheInvalidateAll;
+import io.quarkus.cache.CacheResult;
+
+@ApplicationScoped
+public class MeuServicoDemorado {
+
+    private AtomicInteger inteiro = new AtomicInteger();
+
+    // @Cacheable(value = "nome-do-cache")
+    @CacheResult(cacheName = "nome-do-cache")
+    public String primeiroCacheavel(String id, Object entidade) {
+        demore();
+        return "Primeiro Método Cacheavel " + inteiro.getAndIncrement() + ", ID: " + id;
+    }
+
+    // @CacheEvict(value = "nome-do-cache", allEntries = true)
+    @CacheInvalidateAll(cacheName = "nome-do-cache")
+    public void limpePrimeiroCacheTodo() {
+    }
+
+    // @CacheEvict(value = "nome-do-cache")
+    @CacheInvalidate(cacheName = "nome-do-cache")
+    public void limpePrimeiroCache(String id, Object entidade) {
+    }
+
+    // @CachePut(value = "nome-do-cache")
+    // Não temos uma anotação similar
+    public String segundoCacheavel(String id, Object entidade) {
+        demore();
+        return "Segundo Cacheavel " + inteiro.getAndIncrement() + ", ID: " + id;
+    }
+
+    private void demore() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
